547dcd54a17f65fc49111b752da09d456e1b87fc
pitot <gkstn20425@gmail.com>
2020-09-10 06:33:32 +0900

    HonGong1

---
+++ HonGong1/Example.C
@@ -0,0 +1,13 @@
+//예시파일로서 Hello World! 의 구동을 사용
+//>null 명령어를 통해 순수하게 해당 영문장만 나오도록 코딩함
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+	printf("Hello World!");
+	system("pause>null");
+
+	return 0;
+}
