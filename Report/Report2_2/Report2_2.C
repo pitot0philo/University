359cd752d7f29c465a7c4d624b6df7de589360b5
pitot <gkstn20425@gmail.com>
2020-09-15 15:29:11 +0900

    BMI 출력

--- Report/Report2_2/Report2_2.C
+++ Report/Report2_2/Report2_2.C
@@ -1,11 +1,17 @@
 #include <stdio.h>
+#include <math.h>
 int main(void)
 {
-	int a;
-	double b;
-	printf("몸무게(kg)와 키(cm)를 입력:");
-	scanf("%d%lf", &a, &b);
-	printf("입력된 값: %d%lf\n", a, b);
+	double weight;
+	double height;
+	double c, d, e;
 
+	printf("몸무게(kg)와 키(cm)를 입력");
+	scanf("%lf%lf", &weight, &height);
+	
+	c = height / 100;
+	d = powf(c, 2);
+	e = weight / d;
+	printf("몸무게는 %lf kg, 키는 %.2lf m이다. BMI 지수는 %.3lf이다", weight, c, e);
 	return 0;
 }
