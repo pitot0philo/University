2d0984a7d7d3b2164315583a9db714b39dd312fe
pitot <gkstn20425@gmail.com>
2020-09-16 12:12:59 +0900

    사칙연산 완전 추가

--- Report/Report2_3/HonGong2/HonGong.C
+++ Report/Report2_3/HonGong2/HonGong.C
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <math.h>
+#include <stdlib.h>
 
 int main(void)
 {
@@ -7,8 +8,12 @@ int main(void)
 	printf("Á¤¼ö µÎ°³¸¦ ÀÔ·Â:");
 	scanf("%d%d", &a, &b);
 	
+	printf("µ¡¼À : %d\n", a + b);
+	printf("»¬¼À : %d\n", a - b);
+	printf("°ö¼À : %d\n", a * b);
 	if (b != 0)//ºÐ¸ð°¡ 0ÀÌ µÇÁö ¾Ê°Ô ¹æÁö
-		printf("%d / %d=%.4f", a, b, a / (float)b);
+		printf("³ª´°¼À : %d / %d=%.4f", a, b, a / (float)b);
 		
-		return 0;
+	system("pause>null");
+	return 0;
 }
