9545bb05c1e53741b0b341ee5d3e3356eb0215ac
pitot <gkstn20425@gmail.com>
2020-09-18 11:16:54 +0900

    scanf 반환값 처리

---
+++ Report/Report3_1/Report3_1_A/Report3_1_A/Report3_1_A.C
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(void)
+{
+	double height = 179.5;
+	double weight = 75.0;
+	if (187.5 <= height && weight <= 80)
+	{
+		printf("ok\n");
+	}
+	else
+	{
+		printf("cancle\n");
+	}
+	return 0;
+}
---
+++ Report/Report3_2/HonGong.C
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+	int A, B;
+	int a, b;
+
+	a = scanf("%d", &A);
+	b = scanf("%d", &B);
+
+	printf("%d\n%d\n", A, B);
+	printf("%d\n%d\n", a, b);
+
+	return 0;
+}
