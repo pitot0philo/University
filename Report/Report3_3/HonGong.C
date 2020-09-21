90d38b71895e07d17eb3f8df8d57a4b6b3f8aa13
pitot <gkstn20425@gmail.com>
2020-09-21 22:13:41 +0900

    Report 3-3 done

---
+++ Report/Report3_3/HonGong.C
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int a, b;
+	printf("Input 2 integer variable  : ");
+	scanf("%d%d", &a, &b);
+
+	if (a % 2 == 0 && b % 2 == 0)
+	{
+		printf("%d and %d are all even number", a, b);
+	}
+	else if (a % 2 == 0 && b % 2 != 0)
+	{
+		printf("%d is even number but %d is odd number", a, b);
+	}
+	else if (a % 2 != 0 && b % 2 == 0)
+	{
+		printf("%d is even number but %d is odd number", b, a);
+	}
+	else
+	{
+		printf("%d and %d are all odd number", a, b);
+	}
+
+	return 0;
+}
+
+//if-else 단독으로 사용한 것은, a,b를 모두 평가해야 하는데, a는 1이고 b는 0, 혹은 그 반대의 경우에는 switch 구문으로
+//나타내기 힘들다는 문제가 생긴다. 이에 의해서 && 연산자로 단순히 나타낼 수 있는 if-else를 단독으로 사용하였다.
