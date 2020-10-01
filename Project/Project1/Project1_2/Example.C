b6a347a51951c2407045a82f4c3b39c0617e720a
pitot <gkstn20425@gmail.com>
2020-10-01 21:20:23 +0900

    Project1_2 modified

--- Project/Project1/Project1_2/Example.C
+++ Project/Project1/Project1_2/Example.C
@@ -6,31 +6,31 @@ int main(void)
 	printf("Enter month : ");
 	scanf("%d", &a);
 
-	//1월, 2월, 3월 겨울
-	//4월, 5월, 6월 봄
-	//7월, 8월, 9월 여름
-	//10월 11월 12월 가을
+	//12월, 1월, 2월 겨울
+	//3월, 4월, 5월 봄
+	//6월, 7월, 8월 여름
+	//9월 10월 11월 가을
 	if (a > 12 || a < 0)
 	{
 		printf("Invaild Value!");
 	}
 	else
 	{
-		if (a <= 3 && a >= 1)
-		{
-			printf("Winter");
-		}
-		else if (a <= 6 && a >= 4)
+		if (a <= 5 && a >= 3)
 		{
 			printf("Spring");
 		}
-		else if (a <= 9 && a >= 7)
+		else if (a <= 8 && a >= 6)
 		{
 			printf("Summer");
 		}
+		else if (a <= 11 && a >= 9)
+		{
+			printf("Autumn");
+		}
 		else
 		{
-			printf("Fall");
+			printf("Winter");
 		}
 	}
 
