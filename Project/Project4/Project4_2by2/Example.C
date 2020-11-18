16b1c3f87a5b88ec538a785b54ee2e2fb04b2e26
pitot0philo <gkstn20425@gmail.com>
2020-11-18 16:32:37 +0900

    Project4_2by2 done

--- Project/Project4/Project4_2by2/Example.C
+++ Project/Project4/Project4_2by2/Example.C
@@ -38,15 +38,56 @@ void print_submatrix(double(*MatArry1)[2], double(*MatArry2)[2], int col, int ro
 	}
 }
 
+void print_multmatrix(double(*MatArry1)[2], double(*MatArry2)[2], int col, int row)
+{
+	for (int i = 0; i < row; i++)
+	{
+		for (int j = 0; j < col; j++)
+		{
+			printf("%.4lf  ", MatArry1[i][0]*MatArry2[0][j]+MatArry1[i][1]*MatArry2[1][j]);
+		}
+		printf("\n");
+	}
+}
+
+void print_trans(double(*MatArry)[2], int col, int row)
+{
+	for (int i = 0; i < row; i++)
+	{
+		for (int j = 0; j < col; j++)
+		{
+			printf("%.4lf  ", MatArry[j][i]);
+		}
+		printf("\n");
+	}
+}
+
+void print_inver(double(*MatArry)[2], int col, int row)
+{
+	double Det = 0;
+	Det = MatArry[0][0] * MatArry[1][1] - MatArry[0][1] * MatArry[1][0];
+
+	for (int i = 0; i < row; i++)
+	{
+		for (int j = 0; j < col; j++)
+		{
+			if ((i + 1) % 2 != (j + 1) % 2)
+			{
+				printf("%.4lf  ", -MatArry[(j + 1) % 2][(i + 1) % 2] / Det);
+			}
+			else
+			{
+				printf("%.4lf  ", MatArry[(j + 1) % 2][(i + 1) % 2] / Det);
+			}
+		}
+		printf("\n");
+	}
+}
 int main(void)
 {
 	int i, j, k, m;
 	double matrix1[2][2];
 	double matrix2[2][2];
-	double subt_matrix[2][2];
-	double mult_matrix[2][2];
-	double trans_matrix1[2][2];
-	double trans_matrix2[2][2];
 
 	srand((unsigned)time(NULL));
 
@@ -83,97 +124,23 @@ int main(void)
 	printf("\nSubtraction of matrix 1 and matrix 2 is : \n");
 	print_submatrix(matrix1, matrix2, col, row);
 
-	//행렬의 곱셈:  a11 a12 a13       b11 b12 b13		a11*b11+a12*b21+a13*b31  a11*b12+a12*b22+a13+b32  a11*b13+a12*b23+a13*b33
-	//  			a21 a22 a23	  x   b21 b22 b23	=	a21*b11+a22*b21+a23*b31  a21*b12+a22*b22+a23*b32  a21*b13+a22*b23+a23*b33
-	//				a31 a32 a33		  b31 b32 b33		a31*b11+a32*b21+a33*b31  a31*b12+a32*b22+a33*b32  a31*b13+a32*b23+a33*b33
-	//등호 왼쪽은 곱셈으로 새로 만들어진 행렬 C, 오른쪽에서 곱셈기호 왼쪽은 a, 오른쪽은 b이면
-	//11=11*11+12*21+13*31  12=11*12+12*22+13*32 13=11*13+12*23+13*33...이므로
-	//C행렬이 ij일때 곱은 ikkj로 반복.  3중 for문으로 ijk 사용, matrix5[i][j]=matrix1[i][k]*matrix2[k][j]
-
+	//행렬의 곱셈
 	printf("\nmultiply of matrix 1 and matrix 2 is : \n");
-	for (i = 0; i < 2; i++)
-	{
-		for (j = 0; j < 2; j++)
-		{
-			mult_matrix[i][j] = 0;
-			for (k = 0; k < 2; k++)
-			{
-				mult_matrix[i][j] += matrix1[i][k] * matrix2[k][j];
-			}
-			printf("%.4lf  ", mult_matrix[i][j]);
-		}
-		printf("\n");
-	}
+	print_multmatrix(matrix1, matrix2, col, row);
 
+	//행렬의 전치
 	printf("\ntranspose of matrix 1 is : \n");
-
-	for (i = 0; i < 2; i++)
-	{
-		for (j = 0; j < 2; j++)
-		{
-			trans_matrix1[i][j] = matrix1[j][i];
-			printf("%.4lf  ", trans_matrix1[i][j]);
-		}
-		printf("\n");
-	}
+	print_trans(matrix1, col, row);
 
 	printf("\ntranspose of matrix 2 is : \n");
+	print_trans(matrix2, col, row);
 
-	for (i = 0; i < 2; i++)
-	{
-		for (j = 0; j < 2; j++)
-		{
-			trans_matrix2[i][j] = matrix2[j][i];
-			printf("%.4lf  ", trans_matrix2[i][j]);
-		}
-		printf("\n");
-	}
-
+	//역행렬
 	printf("\nInverse of matrix 1 is : \n");
-
-	double inv_matrix1[2][2];
-	double det_matrix1;
-	det_matrix1 = matrix1[0][0] * matrix1[1][1] - matrix1[0][1] * matrix1[1][0];
-
-	for (i = 0; i < 2; i++)
-	{
-		for (j = 0; j < 2; j++)
-		{
-			if ((i + 1) % 2 != (j + 1) % 2)
-			{
-				inv_matrix1[i][j] = -matrix1[(j + 1) % 2][(i + 1) % 2] / det_matrix1;
-			}
-			else
-			{
-				inv_matrix1[i][j] = matrix1[(j + 1) % 2][(i + 1) % 2] / det_matrix1;
-			}
-			printf("%.4lf  ", inv_matrix1[i][j]);
-		}
-		printf("\n");
-	}
+	print_inver(matrix1, col, row);
 
 	printf("\nInverse of matrix 2 is : \n");
-
-	double inv_matrix2[2][2];
-	double det_matrix2;
-	det_matrix2 = matrix2[0][0] * matrix2[1][1] - matrix2[0][1] * matrix2[1][0];
-
-	for (i = 0; i < 2; i++)
-	{
-		for (j = 0; j < 2; j++)
-		{
-			if ((i + 1) % 2 != (j + 1) % 2)
-			{
-				inv_matrix2[i][j] = -matrix2[(j + 1) % 2][(i + 1) % 2] / det_matrix2;
-			}
-			else
-			{
-				inv_matrix2[i][j] = matrix2[(j + 1) % 2][(i + 1) % 2] / det_matrix2;
-			}
-			printf("%.4lf  ", inv_matrix2[i][j]);
-		}
-		printf("\n");
-	}
+	print_inver(matrix2, col, row);
 
 	return 0;
 }
